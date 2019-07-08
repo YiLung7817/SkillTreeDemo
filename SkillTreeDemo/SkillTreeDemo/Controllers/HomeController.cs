c21537491333338e5b54773d9ef0df070e83440a
YILUNG CHEN <lung7817@icloud.com>
2019-07-08 23:53:10 +0800

    Homework1

---
+++ SkillTreeDemo/SkillTreeDemo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using SkillTreeDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkillTreeDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<AccountingViewModel> accountingList = new List<AccountingViewModel>();
+
+            Random random = new Random();
+            for (int i = 1; i <= 100; i++)
+            {
+                accountingList.Add(new AccountingViewModel()
+                {
+                    Num = i,
+                    Type = random.Next(1, 3).ToString(),
+                    Amount = random.Next(1, 10000).ToString("#,0"),
+                    Date = DateTime.Now,
+                    Note = string.Empty
+                });
+            }
+            return View(accountingList);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
